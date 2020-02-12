c1eae9496f351d26ba268d15d6572e3ad5341547
fdolzanes1 <fdolzanes1@gmail.com>
2020-02-12 11:31:20 -0400

    MongoNetworkError

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ app.use(express.json());
 var urlhost = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb://localhost:27017/contacts";
 var theport = process.env.PORT || 5000;
 //Iniciando DB
-mongoose.connect(urlhost, function(err, res) {
-	if (err) {
-    	console.log ('ERROR connecting to: ' + urlhost + '. ' + err);
-    } else {
-    	console.log ('Succeeded connected to: ' + urlhost);
-    }
-});
+mongoose.connect(urlhost, 
+  {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true,     
+    useCreateIndex: true 
+  }
+);
+
 requireDir('./src/models');
 
 //Rotas
