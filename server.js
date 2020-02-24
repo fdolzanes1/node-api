4cd951b2b124659577a05a91c25852c2352b7a48
fdolzanes1 <fdolzanes1@gmail.com>
2020-02-24 19:32:04 -0400

    add routes in cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 //Cors
 app.use(cors());
 
-app.use(function(req, res, next) {
+app.use(require('./src/routes'), function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
